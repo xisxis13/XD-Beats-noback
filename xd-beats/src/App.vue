08bc8a18e277c2596208cc5b940d2aeb62f08e67
g62664 <62664@etu.he2b.be>
2025-03-27 16:13:20 +0100

    add page layout + common style + font

--- xd-beats/src/App.vue
+++ xd-beats/src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import('./assets/style/font.css');
+import('./assets/style/common.css');
 </script>
 
 <template>
--- xd-beats/src/layouts/PageLayout.vue
+++ xd-beats/src/layouts/PageLayout.vue
@@ -7,4 +7,25 @@ import { RouterView } from 'vue-router'
   <h1>I am a layout</h1>
 </template>
 
+<style>
+#app {
+  display: grid;
+  grid-template-areas:
+  "menu search settings"
+  "menu main now"
+  "menu player player";
+  grid-template-columns: 200px 1fr 250px;
+  grid-template-rows: 56px 1fr 90px;
+  column-gap: 12px;
+  row-gap: 12px;
 
+  padding: 12px;
+  width: 100vw;
+  height: 100vh;
+
+  background-color: var(--background);
+  color: var(--white);
+
+  overflow: hidden;
+}
+</style>
